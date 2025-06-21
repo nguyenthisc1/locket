4b997d2e977b69d4708a0f5bc7d96d3fc61c5701
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-06-21 23:16:48 +0700

    update: splash page

--- lib/presentation/splash/pages/onboarding_page.dart
+++ lib/presentation/splash/pages/onboarding_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:locket/common/helper/navigation/app_navigation.dart';
+import 'package:locket/core/configs/assets/app_images.dart';
+import 'package:locket/core/configs/theme/index.dart';
 import 'package:locket/presentation/auth/pages/login_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -15,19 +18,76 @@ class _OnboardingPageState extends State<OnboardingPage> {
     await prefs.setBool('onboarding_complete', true);
 
     if (!mounted) return;
+  }
 
-    Navigator.of(
-      context,
-    ).pushReplacement(MaterialPageRoute(builder: (_) => const LoginPage()));
+  @override
+  void initState() {
+    super.initState();
+    _completeOnboarding();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: _completeOnboarding,
-          child: const Text('Get Started'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: AppDimensions.xxl),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(AppDimensions.radiusXl),
+                    child: Image.asset(
+                      AppImages.logo,
+                      width: AppDimensions.avatarLg,
+                      height: AppDimensions.avatarLg,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  const SizedBox(width: AppDimensions.md),
+                  Text('Locket', style: AppTypography.displayLarge),
+                ],
+              ),
+              const SizedBox(height: AppDimensions.lg),
+              Text(
+                'Theo dõi ảnh từ bạn bè, trên màn hình chính của bạn',
+                style: AppTypography.displayMedium.copyWith(
+                  color: AppColors.textSecondary,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: AppDimensions.xl),
+              ElevatedButton(
+                onPressed: () => AppNavigator.push(context, const LoginPage()),
+                style: ElevatedButton.styleFrom(
+                  textStyle: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                child: Text('Tạo tài khoản'),
+              ),
+              const SizedBox(height: AppDimensions.md),
+              ElevatedButton(
+                onPressed: () => AppNavigator.push(context, const LoginPage()),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  textStyle: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                child: Text(
+                  'Đăng nhập',
+                  style: TextStyle(color: AppColors.textSecondary),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/presentation/splash/pages/splash_page.dart
+++ lib/presentation/splash/pages/splash_page.dart
@@ -1,41 +1,12 @@
 // splash_page.dart
 import 'package:flutter/material.dart';
 import 'package:locket/common/wigets/auth_gate.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'onboarding_page.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
-  Future<bool> checkFirstLaunch() async {
-    final prefs = await SharedPreferences.getInstance();
-    final isFirstLaunch = prefs.getBool('isFirstLaunch') ?? true;
-
-    if (isFirstLaunch) {
-      await prefs.setBool('isFirstLaunch', false);
-    }
-
-    return isFirstLaunch;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<bool>(
-      future: checkFirstLaunch(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return const Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        }
-
-        final isFirstLaunch = snapshot.data!;
-        if (isFirstLaunch) {
-          return const OnboardingPage();
-        } else {
-          return const AuthGate();
-        }
-      },
-    );
+    return AuthGate();
   }
 }
