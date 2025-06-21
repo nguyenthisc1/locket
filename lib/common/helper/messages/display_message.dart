7ee56600845c6dd5d23f73d0443c449ac121d287
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-06-21 23:58:20 +0700

    update: display message

--- lib/common/helper/messages/display_message.dart
+++ lib/common/helper/messages/display_message.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
 class DisplayMessage {
-  static void errorMessage(String message, BuildContext context) {
+  static void error(BuildContext context, String message) {
     var snackbar = SnackBar(
       content: Text(message),
       behavior: SnackBarBehavior.floating,
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
+  static void success(BuildContext context, String message) {
+    var snackbar = SnackBar(
+      content: Text(message),
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: Colors.green,
     );
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
