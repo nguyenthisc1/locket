76e035bdb883bc426a1d3dc1e3ed5e139b31127b
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-06-21 23:16:07 +0700

    add: firebase option

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:locket/core/constants/routes.dart';
+import 'package:locket/firebase_option.dart';
+import 'package:locket/presentation/splash/pages/onboarding_page.dart';
 import 'package:locket/presentation/splash/pages/splash_page.dart';
-import 'package:logger/web.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/configs/theme/index.dart';
@@ -10,7 +11,7 @@ import 'core/configs/theme/index.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   final prefs = await SharedPreferences.getInstance();
   final onboardingComplete = prefs.getBool('onboarding_complete') ?? false;
@@ -27,9 +28,10 @@ class MyApp extends StatelessWidget {
       title: 'Locket Clone',
       theme: AppTheme.light(),
       darkTheme: AppTheme.dark(),
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.dark,
       home: const SplashPage(),
       initialRoute: onboardingComplete ? '/auth' : '/onboarding',
+      debugShowCheckedModeBanner: false,
       routes: appRoutes,
     );
   }
