92e62e67a647626408639d99a0882ef0f23ea714
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-07-06 09:56:49 +0700

    update: routes - description: add converstion

--- lib/core/constants/routes.dart
+++ lib/core/constants/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:locket/common/wigets/auth_gate.dart';
+import 'package:locket/presentation/conversation/pages/conversation_page.dart';
 import 'package:locket/presentation/home/pages/home_page.dart';
 import 'package:locket/presentation/splash/pages/onboarding_page.dart';
 
@@ -7,4 +8,5 @@ final Map<String, WidgetBuilder> appRoutes = {
   '/onboarding': (_) => const OnboardingPage(),
   '/auth': (_) => const AuthGate(),
   '/home': (_) => const HomePage(),
+  '/converstion': (_) => const ConversationPage(),
 };
