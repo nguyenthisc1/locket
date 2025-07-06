eb154140dc5468922b9e195011eb459a42ed641a
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-07-06 10:06:26 +0700

    update: home controller

---
+++ lib/presentation/home/controllers/home_controller.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class HomeControllerState extends ChangeNotifier {
+  final PageController outerController = PageController();
+  final PageController innerController = PageController();
+
+  int _currentOuterPage = 0;
+  bool _enteredFeed = false;
+
+  // Getters
+  int get currentOuterPage => _currentOuterPage;
+  bool get enteredFeed => _enteredFeed;
+
+  void init() {
+    outerController.addListener(_outerPageListener);
+  }
+
+  void _outerPageListener() {
+    final int newPage =
+        outerController.hasClients ? outerController.page?.round() ?? 0 : 0;
+
+    if (newPage != _currentOuterPage) {
+      if (_currentOuterPage == 0 && newPage == 1) {
+        _enteredFeed = true;
+        notifyListeners();
+      }
+
+      if (_currentOuterPage == 1 && newPage == 0) {
+        _enteredFeed = false;
+        notifyListeners();
+      }
+
+      _currentOuterPage = newPage;
+    }
+  }
+
+  void handleScrollPageViewOuter(int page) {
+    if (outerController.hasClients) {
+      outerController.animateToPage(
+        page,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    outerController.removeListener(_outerPageListener);
+    outerController.dispose();
+    innerController.dispose();
+    super.dispose();
+  }
+}
--- lib/presentation/home/pages/home_page.dart
+++ lib/presentation/home/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:locket/common/wigets/appbar/appbar.dart';
 import 'package:locket/common/wigets/user_image.dart';
 import 'package:locket/core/configs/theme/app_dimensions.dart';
+import 'package:locket/presentation/home/controllers/home_controller.dart';
 import 'package:locket/presentation/home/widgets/camera/index.dart';
 import 'package:locket/presentation/home/widgets/feed/feed.dart';
 import 'package:locket/presentation/home/widgets/friend_select.dart';
@@ -17,122 +18,94 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final PageController _outerController = PageController();
-  final PageController _innerController = PageController();
-
-  int _currentOuterPage = 0;
-  bool _enteredFeed = false;
+  late final HomeControllerState _homeController;
 
   @override
   void initState() {
     super.initState();
-    _outerController.addListener(_outerPageListener);
-  }
-
-  void _outerPageListener() {
-    final int newPage =
-        _outerController.hasClients ? _outerController.page?.round() ?? 0 : 0;
-
-    if (newPage != _currentOuterPage) {
-      if (_currentOuterPage == 0 && newPage == 1) {
-        setState(() {
-          _enteredFeed = true;
-        });
-      }
-
-      if (_currentOuterPage == 1 && newPage == 0) {
-        setState(() {
-          _enteredFeed = false;
-        });
-      }
-
-      _currentOuterPage = newPage;
-    }
-  }
-
-  void _handleScrollPageViewOuter(int page) {
-    if (_outerController.hasClients) {
-      _outerController.animateToPage(
-        page,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
-    }
+    _homeController = HomeControllerState();
+    _homeController.init();
   }
 
   @override
   void dispose() {
-    _outerController.removeListener(_outerPageListener);
-    _outerController.dispose();
-    _innerController.dispose();
+    _homeController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: BasicAppbar(
-        hideBack: true,
-        action: Expanded(
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: AppDimensions.md,
-
-              right: AppDimensions.md,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const UserImage(),
-                _enteredFeed ? const FriendSelect() : const FriendTopbar(),
-                const MessButton(),
-              ],
+    return ListenableBuilder(
+      listenable: _homeController,
+      builder: (context, child) {
+        return Scaffold(
+          extendBodyBehindAppBar: true,
+          appBar: BasicAppbar(
+            hideBack: true,
+            action: Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: AppDimensions.md,
+                  right: AppDimensions.md,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const UserImage(),
+                    _homeController.enteredFeed
+                        ? const FriendSelect()
+                        : const FriendTopbar(),
+                    const MessButton(),
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
-      ),
-      body: PageView.builder(
-        controller: _outerController,
-        scrollDirection: Axis.vertical,
-        itemCount: 2,
-        itemBuilder: (context, index) {
-          if (index == 0) {
-            return Stack(
-              children: [
-                // CAMERA REVIEW
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: AppDimensions.md,
-                    right: AppDimensions.md,
-                    top: AppDimensions.appBarHeight + AppDimensions.xl,
-                  ),
-                  child: Camera(),
-                ),
+          body: PageView.builder(
+            controller: _homeController.outerController,
+            scrollDirection: Axis.vertical,
+            itemCount: 2,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return Stack(
+                  children: [
+                    // CAMERA REVIEW
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: AppDimensions.md,
+                        right: AppDimensions.md,
+                        top: AppDimensions.appBarHeight + AppDimensions.xl,
+                      ),
+                      child: Camera(),
+                    ),
 
-                // HISTORY BUTTON
-                Positioned(
-                  bottom: AppDimensions.xl,
-                  left: 0,
-                  right: 0,
-                  child: GestureDetector(
-                    onTap: () => _handleScrollPageViewOuter(1),
-                    child: Center(child: HistoryFeed()),
-                  ),
-                ),
-              ],
-            );
-          }
+                    // HISTORY BUTTON
+                    Positioned(
+                      bottom: AppDimensions.xl,
+                      left: 0,
+                      right: 0,
+                      child: GestureDetector(
+                        onTap:
+                            () => _homeController.handleScrollPageViewOuter(1),
+                        child: Center(child: HistoryFeed()),
+                      ),
+                    ),
+                  ],
+                );
+              }
 
-          // FEEDS
-          return Feed(
-            innerController: _innerController,
-            outerController: _outerController,
-            handleScrollFeedToTop: () => _handleScrollPageViewOuter(0),
-          );
-        },
-      ),
+              // FEEDS
+              return Feed(
+                innerController: _homeController.innerController,
+                outerController: _homeController.outerController,
+                handleScrollFeedToTop:
+                    () => _homeController.handleScrollPageViewOuter(0),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
