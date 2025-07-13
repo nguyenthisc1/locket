5d7238e80248365445a967a347df335c8bbcbf59
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-07-13 20:44:31 +0700

    add: fade animation

---
+++ lib/common/animations/fade_animation_controller.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class FadeAnimationController {
+  late final AnimationController controller;
+  late final Animation<double> animation;
+
+  FadeAnimationController({
+    required TickerProvider vsync,
+    Duration duration = const Duration(milliseconds: 300),
+    Curve curve = Curves.easeInOut,
+  }) {
+    controller = AnimationController(vsync: vsync, duration: duration);
+
+    controller.value = 1;
+    animation = CurvedAnimation(parent: controller, curve: curve);
+  }
+
+  void fadeIn() {
+    controller.forward();
+  }
+
+  void fadeOut() {
+    controller.reverse();
+  }
+
+  void dispose() {
+    controller.dispose();
+  }
+}
