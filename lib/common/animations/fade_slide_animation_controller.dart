34b01dc774a5cd16b0f71fa6b28f4134e26a8c41
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-07-14 21:44:17 +0700

    add: fade slide animation controller

---
+++ lib/common/animations/fade_slide_animation_controller.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class FadeSlideAnimationController {
+  final TickerProvider vsync;
+  final Duration duration;
+  final Curve curve;
+
+  late AnimationController controller;
+  late Animation<double> fadeAnimation;
+  late Animation<Offset> slideAnimation;
+
+  FadeSlideAnimationController({
+    required this.vsync,
+    this.duration = const Duration(milliseconds: 300),
+    this.curve = Curves.easeInOut,
+  }) {
+    controller = AnimationController(vsync: vsync, duration: duration);
+  }
+
+  /// Khởi tạo hoặc cập nhật animation với hướng translate mong muốn
+  void configure({required Offset beginOffset}) {
+    fadeAnimation = CurvedAnimation(parent: controller, curve: curve);
+
+    slideAnimation = Tween<Offset>(
+      begin: beginOffset,
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: controller, curve: curve));
+  }
+
+  void fadeIn() => controller.forward();
+  void fadeOut() => controller.reverse();
+  void dispose() => controller.dispose();
+}
