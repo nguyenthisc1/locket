8d15f97719718ac17e1b4d14b938b9f62b4d3ea1
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-08-03 23:15:57 +0700

    add: user service

---
+++ lib/core/services/user_service.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:locket/data/user/models/user_profile_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserService {
+  UserProfileModel? _currentUser;
+
+  UserProfileModel? get currentUser => _currentUser;
+
+  bool get isLoggedIn => _currentUser != null;
+
+  /// Sets the current user and persists the user to SharedPreferences.
+  Future<void> setUser(UserProfileModel user) async {
+    _currentUser = user;
+    final prefs = await SharedPreferences.getInstance();
+    final jsonStr = jsonEncode(user.toJson());
+    await prefs.setString('currentUser', jsonStr);
+  }
+
+  /// Loads the user from SharedPreferences, if present.
+  Future<void> loadUserFromStorage() async {
+    final prefs = await SharedPreferences.getInstance();
+    final jsonStr = prefs.getString('currentUser');
+    if (jsonStr != null) {
+      final json = jsonDecode(jsonStr);
+      _currentUser = UserProfileModel.fromJson(json);
+    }
+  }
+
+  /// Clears the current user and removes the user from SharedPreferences.
+  Future<void> clearUser() async {
+    _currentUser = null;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('currentUser');
+  }
+}
