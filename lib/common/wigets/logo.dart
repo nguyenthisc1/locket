ca3a4ebaa71eb755ee480846461efcec2ca88fbe
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-08-10 17:58:51 +0700

    update: logo

--- lib/common/wigets/logo.dart
+++ lib/common/wigets/logo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:locket/common/wigets/ratio_clip.dart';
 import 'package:locket/core/configs/assets/app_images.dart';
 import 'package:locket/core/configs/theme/app_dimensions.dart';
 
@@ -9,8 +10,8 @@ class Logo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(AppDimensions.radiusXl),
+    return RatioClip(
+      radiusRatio: 0.9,
       child: Image.asset(
         AppImages.logo,
         width: size ?? AppDimensions.avatarLg,
