96a224d5010e7b819ca43f412e4ad41a80e11ffd
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-08-20 23:11:50 +0700

    update: update feed use case

--- lib/domain/feed/usecases/upload_feed_usecase.dart
+++ lib/domain/feed/usecases/upload_feed_usecase.dart
@@ -4,11 +4,13 @@ import 'package:locket/core/models/base_response_model.dart';
 import 'package:locket/domain/feed/repositories/feed_repository.dart';
 
 class UploadFeedUsecase {
-    final FeedRepository _feedRepository;
+  final FeedRepository _feedRepository;
 
   UploadFeedUsecase(this._feedRepository);
 
-  Future<Either<Failure, BaseResponse>> call(Map<String, dynamic> payload) async {
+  Future<Either<Failure, BaseResponse>> call(
+    Map<String, dynamic> payload,
+  ) async {
     return await _feedRepository.uploadFeed(payload);
   }
 }
