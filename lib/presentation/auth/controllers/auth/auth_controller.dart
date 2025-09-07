21dc5aec1d4b3ed160faebcc5019ce4e025085ac
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-09-07 14:22:35 +0700

    update: auth - description: loading user and token

--- lib/presentation/auth/controllers/auth/auth_controller.dart
+++ lib/presentation/auth/controllers/auth/auth_controller.dart
@@ -1,4 +1,7 @@
+import 'package:fresh_dio/fresh_dio.dart';
 import 'package:locket/core/services/user_service.dart';
+import 'package:locket/data/auth/models/token_model.dart';
+import 'package:locket/di.dart';
 import 'package:locket/domain/auth/usecase/login_usecase.dart';
 import 'package:locket/presentation/auth/controllers/auth/auth_controller_state.dart';
 import 'package:logger/logger.dart';
@@ -15,12 +18,11 @@ class AuthController {
     required LoginUsecase loginUsecase,
     required UserService userService,
     Logger? logger,
-  }) : _state = state,
-       _loginUsecase = loginUsecase,
-       _userService = userService,
-       _logger =
-           logger ??
-           Logger(printer: PrettyPrinter(colors: true, printEmojis: true));
+  })  : _state = state,
+        _loginUsecase = loginUsecase,
+        _userService = userService,
+        _logger = logger ??
+            Logger(printer: PrettyPrinter(colors: true, printEmojis: true));
 
   // Getters for external access
   AuthControllerState get state => _state;
@@ -28,7 +30,20 @@ class AuthController {
   /// Initialize auth state (check if user is already logged in)
   Future<void> init() async {
     try {
+      final tokenStorage = getIt<TokenStorage<AuthTokenPair>>();
+      final tokenPair = await tokenStorage.read();
+
       await _userService.loadUserFromStorage();
+
+      // If accessToken is null, clear all user data and tokens
+      if (tokenPair?.accessToken == null) {
+        await _userService.clearUser();
+        await tokenStorage.delete();
+        _state.reset();
+        _logger.d('Access token is null. Cleared user, tokens, and state.');
+        return;
+      }
+
       if (_userService.isLoggedIn) {
         // Convert UserProfileModel to UserEntity if needed
         _state.setLoggedIn(true);
